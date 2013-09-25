220dac0c1ea2773ec76d08c3b9422ee31ffd04bb
NguyenHoc <nguyenhoc@xiao.vn>
2013-09-25 13:58:16 +0700

    Nguyễn Học 25/09/2013
    Module view applicaton list

---
+++ views/theme/xpanel/application_list.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Dinh Duy
+ * Date: 9/25/13
+ * Time: 11:28 AM
+ * To change this template use File | Settings | File Templates.
+ ****/
+echo "Danh sach ung dung";
+echo "" .$applist->appname;
+//$dsblog = application::getInstance()->get_blog($_SESSION['xID']);
+$i = 1;
+$active = "";
+foreach(applist as $app)
+{
+    echo $app->appid;
+    echo $app->appname;
+    echo $app->apppermission;
+    $i ++;
+}
