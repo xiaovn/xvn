eb9960a6e7379b9e4e26acbf6c4eab6de798864e
NguyenHoc <nguyenhoc@xiao.vn>
2013-09-25 13:59:51 +0700

    Nguyễn Học 25/09/2013
    Module view applicaton list

--- model/applicationModel.php
+++ model/applicationModel.php
@@ -5,7 +5,7 @@
  * Date: 9/25/13
  * Time: 11:30 AM
  * To change this template use File | Settings | File Templates.
- ***/
+ */
 Class applicationModel Extends baseModel
 {
     public function get_applist()
--- views/theme/xpanel/application_list.php
+++ views/theme/xpanel/application_list.php
@@ -5,7 +5,7 @@
  * Date: 9/25/13
  * Time: 11:28 AM
  * To change this template use File | Settings | File Templates.
- ****/
+ **/
 echo "Danh sach ung dung";
 echo "" .$applist->appname;
 //$dsblog = application::getInstance()->get_blog($_SESSION['xID']);
